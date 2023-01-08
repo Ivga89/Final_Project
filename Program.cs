211102c28e336fc56408f93911f3150ddec4f354
Ivga <borozdinaes89@gmail.com>
2023-01-08 14:46:37 +0500

    основная функция выборки элементов не более 3 символов и сборка программы

--- Program.cs
+++ Program.cs
@@ -26,3 +26,19 @@ void PrintArray(string[] array) //печать массива
     }
 }
 
+string[] ThreeLetterWords (string[] array) {     //функция отбора строк менее 3 символов
+    string[] newArray = new string[array.Length];  //новый массив
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) {             // если символов меньше 3х
+            newArray[i] = array[i];             //запишем в новый массив
+        }
+    } return newArray;
+}
+
+string[] originArray = CreatStringArray();
+System.Console.WriteLine("The array you entered: ");
+PrintArray(originArray);
+System.Console.WriteLine();
+System.Console.WriteLine("Elements of the input array are less than three characters: ");
+PrintArray(ThreeLetterWords(originArray));
